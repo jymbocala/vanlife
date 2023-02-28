b577f5fd30abc01b7384d6e220c8ef1b5908ce40
Jym Bocala <jym.bocala@gmail.com>
2023-02-28 23:13:29 +1100

    create Vans component

--- src/index.js
+++ src/index.js
@@ -2,22 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./index.scss";
-import About from "./pages/About";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Vans from "./pages/Vans";
 
 function App() {
   return (
     <BrowserRouter>
       <React.StrictMode>
         <header>
-          <Link to="/" class="site-logo">#VANLIFE</Link>
+          <Link to="/" class="site-logo">
+            #VANLIFE
+          </Link>
           <nav>
             <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
           </nav>
         </header>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
         </Routes>
       </React.StrictMode>
     </BrowserRouter>
