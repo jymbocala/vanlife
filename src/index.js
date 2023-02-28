1afdb7fb0f1df487787dcf9813911c4eb65e89d2
Jym Bocala <jym.bocala@gmail.com>
2023-02-28 11:15:47 +1100

    add header content and styles

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./index.css";
+// import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <React.StrictMode>
+        <header>
+          <Link to="/" class="nav__logo">#VANLIFE</Link>
+          <nav>
+            <Link to="/about">About</Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </React.StrictMode>
+    </BrowserRouter>
+  );
+}
+function Home() {
+  return <h1>Home Page</h1>;
+}
+function About() {
+  return <h1>About Page</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
