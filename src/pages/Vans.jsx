45dd453fb8c9d58fc8b46fbbf808368ce11b10cd
Jym Bocala <jym.bocala@gmail.com>
2023-03-01 13:46:47 +1100

    map over vans data to create van tile elements

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -12,21 +12,21 @@ export default function Vans() {
       });
   }, []);
 
-  // const vansElements = vans.map(van => {
-  //   return (
-  //     <div key={van.id} className="van-tile">
-  //       <img src={van.imageUrl} alt={`${van.name}`}/>
-  //       <div className="van-info">
-  //         <h3>{van.name}</h3>
-  //         <p>
-  //           ${van.price}
-  //           <span>/day</span>
-  //         </p>
-  //       </div>
-  //       <i className={`van-type ${van.type} selected`}>{van.type}</i>
-  //     </div>
-  //   );
-  // });
+  const vansElements = vans.map(van => {
+    return (
+      <div key={van.id} className="van-tile">
+        <img src={van.imageUrl} alt={`${van.name}`}/>
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </div>
+    );
+  });
 
   return (
     <>
