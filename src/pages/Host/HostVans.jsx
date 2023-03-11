111c6082b4958a5125629963322fab9f3ce5230b
Jym Bocala <jym.bocala@gmail.com>
2023-03-11 14:00:18 +1100

    fetch data from server for api/host/vans and set hostVans state

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
 
 export default function HostVans() {
+  const [hostVans, setHostVans] = useState([]);
+
+  // fetch host/vans route (mirage js intercepts this fetch request to get data from our server)
+  useEffect(() => {
+    fetch("/api/host/vans")
+      .then((res) => res.json())
+      .then((data) => {
+        setHostVans(data.vans);
+      });
+  }, []);
+
+  // //map out hostVans data into jsx elements
+  // const hostVansElements = 
+
+  console.log(hostVans);
+
   return (
     <h1>Host Vans here!</h1>
   )
