2ba82b18537bf638a492ed98f81a8706c84cfa78
Jym Bocala <jym.bocala@gmail.com>
2023-03-11 16:27:08 +1100

    map host vans data into jsx elements

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function HostVans() {
   const [hostVans, setHostVans] = useState([]);
@@ -13,12 +13,30 @@ export default function HostVans() {
       });
   }, []);
 
-  // //map out hostVans data into jsx elements
-  // const hostVansElements = 
+  //map out hostVans data into jsx elements
+  const hostVansElements = hostVans.map((van) => {
+    return (
+      <Link to={`/vans/${van.id}`}>
+        <div key={van.id} className="host-van-row">
+          <img src={van.imageUrl} alt={`${van.name}`} />
+          <div className="host-van-info">
+            <h3>{van.name}</h3>
+            <p>
+              ${van.price}
+              <span>/day</span>
+            </p>
+          </div>
+        </div>
+      </Link>
+    )
+  });
 
   console.log(hostVans);
 
   return (
-    <h1>Host Vans here!</h1>
+    <div className="van-list-container">
+      <h1>Your listed vans</h1>
+      <div className="host-van-list">{hostVansElements}</div>
+    </div>
   )
 }
