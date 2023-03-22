64cc18e130c5f15c44784b257e165bc87225c44a
Jym Bocala <jym.bocala@gmail.com>
2023-03-22 17:57:01 +1100

    create loginUser function

--- src/utility/api.js
+++ src/utility/api.js
@@ -1,3 +1,4 @@
+// function to get vans from api. MirageJS intercepts the fetch
 export async function getVans() {
   const res = await fetch("/api/vans/");
   if (!res.ok) {
@@ -6,7 +7,27 @@ export async function getVans() {
       statusText: res.statusText,
       status: res.status,
     };
-  };
+  }
   const data = await res.json();
   return data.vans;
+}
+
+// function that fetches to the login endpoint
+export async function loginUser(creds) {
+  // creds is an obj that has an email and password property
+  const res = await fetch("/api/login", {
+    method: "post",
+    body: JSON.stringify(creds),
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw {
+      message: data.message,
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
+
+  return data;
 }
