d8ef69129501514e74f69b3ae9126afa420b3056
Jym Bocala <jym.bocala@gmail.com>
2023-03-24 16:04:56 +1100

    add: users model, mock user data, login route

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-import { createServer, Model } from "miragejs";
+import { createServer, Model, Response } from "miragejs";
 
 createServer({
   models: {
     vans: Model,
+    users: Model
   },
 
   seeds(server) {
@@ -72,6 +73,12 @@ createServer({
       type: "rugged",
       hostId: "123",
     });
+    server.create("user", {
+      id: "123",
+      email: "j@m.com",
+      password: "p123",
+      name: "Jym",
+    });
   },
 
   routes() {
@@ -97,6 +104,28 @@ createServer({
       // Hard-code the hostId for now
       const id = request.params.id;
       return schema.vans.where({ id, hostId: "123" });
+    });
+
+    this.post("/login", (schema, request) => {
+      const { email, password } = JSON.parse(request.requestBody);
+      // This is an extremely naive version of authentication. Please don't
+      // do this in the real world, and never save raw text passwords
+      // in your database 😇
+      const foundUser = schema.users.findBy({ email, password });
+      if (!foundUser) {
+        return new Response(
+          401,
+          {},
+          { message: "No user with those credentials found!" }
+        );
+      }
+
+      // At the very least, don't send the password back to the client 😅
+      foundUser.password = undefined;
+      return {
+        user: foundUser,
+        token: "Enjoy your pizza, here's your tokens.",
+      };
     });
   },
 });
