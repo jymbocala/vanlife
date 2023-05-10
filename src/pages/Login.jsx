718a5ef2ec02f87e6f3ca7ca6c6a77046087c4ec
Jym Bocala <jym.bocala@gmail.com>
2023-05-10 23:08:37 +1000

    use useNavigation to access status of log in process

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,11 @@
-import React, { useState } from "react";
-import { useLoaderData, useNavigate, Form, redirect } from "react-router-dom";
+import React from "react";
+import {
+  useLoaderData,
+  Form,
+  redirect,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
 import { loginUser } from "../api";
 
 export function loader({ request }) {
@@ -12,37 +18,22 @@ export async function action({ request }) {
   const formData = await request.formData();
   const email = formData.get("email");
   const password = formData.get("password");
-  const data = await loginUser({ email, password });
-  localStorage.setItem("loggedin", true);
-  console.log(data);
-  return redirect("/host")
+
+  try {
+    const data = await loginUser({ email, password });
+    localStorage.setItem("loggedin", true);
+    console.log(data);
+    return redirect("/host");
+  } catch (err) {
+    return err.message;
+  }
 }
 
 export default function Login() {
   // const location = useLocation(); // this gets the link state from AuthRequired component
-  const [status, setStatus] = useState("idle");
-  const [error, setError] = useState(null);
-  // console.log(status);
+  const status = useNavigation().state;
   const loginFirstMessage = useLoaderData();
-  const navigate = useNavigate();
-
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-  //   setStatus("submitting");
-  //   // setError to null to reset nullify previous errors if this handleSubmit runs more than once.
-  //   setError(null);
-  //   loginUser(loginFormData)
-  //     .then((data) => {
-  //       console.log(data);
-  //       navigate("/host", { replace: true});
-  //     })
-  //     .catch((err) => setError(err))
-  //     // finally block always run whether the promise is resolved or rejected
-  //     .finally(() => {
-  //       // setStatus to idle so that button doesn't stay disabled
-  //       setStatus("idle");
-  //     });
-  // }
+  const errorMessage = useActionData();
 
   return (
     <div className="login-container">
@@ -53,7 +44,7 @@ export default function Login() {
       {loginFirstMessage && (
         <h2 className="login-error">{loginFirstMessage}</h2>
       )}
-      {error && <h3 className="login-error">{error.message}</h3>}{" "}
+      {errorMessage && <h4 className="login-error">{errorMessage}</h4>}{" "}
       <Form method="post" className="login-form" replace>
         <input name="email" type="email" placeholder="Email address" />
         <input name="password" type="password" placeholder="Password" />
